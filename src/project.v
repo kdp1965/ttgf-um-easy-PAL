007e5f3d27cdca59aee9f8aa0620649582417c83
pettitkd <pettitkd@gmail.com>
2025-12-04 04:50:23 -0800

    Increased internal P-terms to 32 and density to 70%.

--- src/project.v
+++ src/project.v
@@ -19,7 +19,7 @@ module tt_um_MATTHIAS_M_PAL_TOP_WRAPPER (
   // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   // PAL size parameters
   parameter NUM_INPUTS = 8;
-  parameter NUM_INTERMEDIATE_STAGES = 30;
+  parameter NUM_INTERMEDIATE_STAGES = 32;
   parameter NUM_OUTPUTS = 7;
   // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
